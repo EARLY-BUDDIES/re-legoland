44663b88e33e78d5540f48e63ec6bcc9ce722967
MinJ33 <erica990604@gmail.com>
2023-05-05 20:11:13 +0900

    Add auto slide animation

--- js/main.js
+++ js/main.js
@@ -86,3 +86,56 @@ async function checkWeather() {
 }
  
 checkWeather();
+
+// Access the Images
+let slideImages = document.querySelectorAll('img');
+
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+
+let dots = document.querySelectorAll('.dot');
+
+let counter = 0;
+
+// Code for prev btn
+next.addEventListener('click', slideNext);
+function slideNext(){
+  slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+  if(counter >= slideImages.length-1){
+    counter = 0;
+  }else{
+    counter++;
+  }
+  slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+}
+
+prev.addEventListener('click', slidePrev);
+function slidePrev(){
+  slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+  if(counter == 0){
+    counter = slideImages.length-1;
+  }else{
+    counter--;
+  }
+  slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+}
+
+// Auto slider
+function autoSliding(){
+  deletInterval = setInterval(timer, 500);
+  function timer(){
+    slideNext();
+  }
+}
+autoSliding();
+
+// Stop auto sliding when mouse is over
+const container = document.querySelector('.slider-container');
+container.addEventListener('mouseover', function(){
+  clearInterval(deletInterval);
+});
+
+// Resume sliding when mouse is out
+container.addEventListener('mouseout', autoSliding);
+
+// Add and remove active class from the indicators
